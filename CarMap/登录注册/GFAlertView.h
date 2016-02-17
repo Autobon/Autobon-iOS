3fe0033acdb36be2e3ecd1a48a15286568e7f411
inCarGithub <363846892@qq.com>
2016-02-17 20:27:42 +0800

    个人信息界面完成，提示框界面代码封装一半

---
+++ CarMap/登录注册/GFAlertView.h
@@ -0,0 +1,13 @@
+//
+//  GFAlertView.h
+//  CarMap
+//
+//  Created by 陈光法 on 16/2/17.
+//  Copyright © 2016年 mll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFAlertView : UIView
+
+@end
---
+++ CarMap/登录注册/GFAlertView.m
@@ -0,0 +1,21 @@
+//
+//  GFAlertView.m
+//  CarMap
+//
+//  Created by 陈光法 on 16/2/17.
+//  Copyright © 2016年 mll. All rights reserved.
+//
+
+#import "GFAlertView.h"
+
+@implementation GFAlertView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
