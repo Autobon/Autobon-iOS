320f44f12bb1481c15f0ba90969f3762cc112b97
inCarGithub <363846892@qq.com>
2016-02-22 11:12:30 +0800

    登录注册全部完成，提示框已经添加，个人信息界面的修改密码完成

---
+++ CarMap/我的信息/GFBillViewController.h
@@ -0,0 +1,13 @@
+//
+//  GFBillViewController.h
+//  CarMap
+//
+//  Created by 陈光法 on 16/2/19.
+//  Copyright © 2016年 mll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFBillViewController : UIViewController
+
+@end
---
+++ CarMap/我的信息/GFBillViewController.m
@@ -0,0 +1,37 @@
+//
+//  GFBillViewController.m
+//  CarMap
+//
+//  Created by 陈光法 on 16/2/19.
+//  Copyright © 2016年 mll. All rights reserved.
+//
+
+#import "GFBillViewController.h"
+
+@interface GFBillViewController ()
+
+@end
+
+@implementation GFBillViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
