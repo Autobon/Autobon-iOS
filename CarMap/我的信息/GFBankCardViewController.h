74f588085756eb4c816bd8dc071d18cf3c23e6f3
inCarGithub <363846892@qq.com>
2016-02-24 11:47:51 +0800

    登录注册，忘记密码BUG修改以及修改密码数据逻辑处理，银行卡参数回调

---
+++ CarMap/我的信息/GFBankCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  GFBankCardViewController.h
+//  CarMap
+//
+//  Created by 陈光法 on 16/2/22.
+//  Copyright © 2016年 mll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFBankCardViewController : UIViewController
+
+@end
