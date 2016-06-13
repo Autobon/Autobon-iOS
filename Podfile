528384286a615fd0f1012177de6bbb7076613c0d
inCarGithub <363846892@qq.com>
2016-06-13 11:18:06 +0800

    no message

--- Podfile
+++ Podfile
@@ -1,4 +1,6 @@
 platform :ios, '7.0'
+target "CarMap" do
 pod 'BaiduMapKit', '~> 2.9.1'
 pod 'GTSDK', '~> 1.3.2'
 pod 'UMengSocial', '~> 4.4'
+end
--- Pods/BaiduMapKit/BaiduMapKit/BaiduMapAPI_Search.framework/Headers/BMKRouteSearchOption.h
+++ Pods/BaiduMapKit/BaiduMapKit/BaiduMapAPI_Search.framework/Headers/BMKRouteSearchOption.h
@@ -14,9 +14,9 @@
     BMKPlanNode        *_from;
     BMKPlanNode        *_to;
 }
-///检索的起点，可通过关键字、坐标两种方式指定。cityName和cityID同时指定时，优先使用cityID
+///检索的起点，可通过关键字、坐标两种方式指定
 @property (nonatomic, strong) BMKPlanNode *from;
-///检索的终点，可通过关键字、坐标两种方式指定。cityName和cityID同时指定时，优先使用cityID
+///检索的终点，可通过关键字、坐标两种方式指定
 @property (nonatomic, strong) BMKPlanNode *to;
 @end
 /// 步行查询基础信息类
@@ -53,8 +53,5 @@
 
 @end
 
-/// 骑行查询基础信息类
-@interface BMKRidingRoutePlanOption : BMKBaseRoutePlanOption
 
-@end
 
